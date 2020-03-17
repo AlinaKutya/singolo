e46900c96c75a377939145939fe1fd66258853fd
Alina Kutyeva <flytta.dig@mail.ru>
2020-03-17 20:54:24 +0300

    feat: add singolo DOM

---
+++ script.js
@@ -0,0 +1,154 @@
+//variables
+const NAV = document.querySelector('.navbar > ul');
+let images = document.querySelector('.portfolio-images');
+let portfolioMenu = document.querySelector('.portfolio-menu');
+let portfolio = document.querySelector('.portfolio-images');
+
+// navbar menu selector
+NAV.addEventListener('click', event => {
+  NAV.querySelectorAll('li > a').forEach(el =>
+    el.classList.remove('navbar-active')
+  );
+  event.target.classList.add('navbar-active');
+});
+
+//portfolio picture outline
+images.addEventListener('click', event => {
+  images.querySelectorAll('img').forEach(el => (el.style.outline = 'none'));
+  if (event.target == images) {
+    return;
+  }
+  event.target.style.outline = '5px solid #F06C64';
+});
+
+// portfolio menu selector and images randomizer
+portfolioMenu.addEventListener('click', event => {
+  if (event.target == portfolioMenu) {
+    return;
+  }
+  portfolioMenu
+    .querySelectorAll('button')
+    .forEach(el => el.classList.remove('portfolio-active'));
+  event.target.classList.add('portfolio-active');
+
+  let picturesNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  picturesNumbers.sort(function() {
+    return 0.5 - Math.random();
+  });
+  for (let i = 0; i < 12; i++) {
+    portfolio.children[i].outerHTML =
+      '<img class="portfolio-image" src="assets/img/Project' +
+      picturesNumbers[i] +
+      '.png" alt="">';
+  }
+});
+
+//main slider
+let items = document.querySelectorAll('.carousel .item');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+  currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+  isEnabled = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('active', direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add('next', direction);
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+}
+
+function nextItem(n) {
+  hideItem('to-left');
+  changeCurrentItem(n + 1);
+  showItem('from-right');
+}
+
+function previousItem(n) {
+  hideItem('to-right');
+  changeCurrentItem(n - 1);
+  showItem('from-left');
+}
+
+document.querySelector('.control.left').addEventListener('click', function() {
+  if (isEnabled) {
+    previousItem(currentItem);
+  }
+  changeBackground();
+});
+
+document.querySelector('.control.right').addEventListener('click', function() {
+  if (isEnabled) {
+    nextItem(currentItem);
+  }
+  changeBackground();
+});
+
+document
+  .querySelector('.transparent-vertical')
+  .addEventListener('click', function() {
+    document.querySelector('.black-vertical').classList.toggle('shown');
+  });
+
+document
+  .querySelector('.transparent-horizontal')
+  .addEventListener('click', () => {
+    document.querySelector('.black-horizontal').classList.toggle('shown');
+  });
+
+function changeBackground() {
+  document.querySelector('.wrapper-carousel').classList.toggle('blue');
+}
+
+// form sending
+
+const BUTTON = document.querySelector('.submit-button');
+const CLOSE_BUTTONS = document.querySelectorAll('.close-button');
+const MESSAGES = document.querySelectorAll('.message-block');
+
+BUTTON.addEventListener('click', () => {
+  let subject = document.getElementById('subject').value.toString();
+  let description = document.getElementById('description').value.toString();
+
+  if (
+    !document.getElementById('email').value ||
+    !document.getElementById('name').value
+  ) {
+    document.querySelector('.message-error').classList.remove('hidden');
+    return;
+  } else {
+    if (!subject) {
+      document.getElementById('subject-result').innerText = 'Without subject';
+    } else {
+      document.getElementById('subject-result').innerText =
+        'Subject: ' + subject;
+    }
+
+    if (!description) {
+      document.getElementById('description-result').innerText =
+        'Without description';
+    } else {
+      document.getElementById('description-result').innerText =
+        'Description: ' + description;
+    }
+
+    document.querySelector('.message-block').classList.remove('hidden');
+  }
+});
+
+CLOSE_BUTTONS.forEach(btn => {
+  btn.addEventListener('click', () =>
+    MESSAGES.forEach(el => el.classList.add('hidden'))
+  );
+});
